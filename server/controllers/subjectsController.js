918f312469bddab4eca760f6ee41afa31f03af93
Bemnet-21 <anjejobemnet@gmail.com>
2025-12-12 23:16:31 +0300

    add a get endpoint for subject

--- server/controllers/subjectsController.js
+++ server/controllers/subjectsController.js
@@ -28,3 +28,22 @@ export const addSubject = async (req, res) => {
         res.status(500).json({ message : "Internal server error" })
     }
 }
+
+export const getSubject = async (req, res) => {
+    const { subjectId } = req.params
+    if(!subjectId) return res.status(400).json({ message : "Subject ID is required" })
+
+    try {
+        const result = await db.query(`SELECT name FROM subject WHERE id = $1`, [subjectId])
+        if(result.rows.length === 0) {
+            return res.status(404).json({ message : "Subject not found" })
+        }
+        res.status(200).json({ 
+            message: "Teacher found",
+            data: result.rows[0]
+         })
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message : "Internal server error" })
+    }
+}
--- server/routes/subjectsRoute.js
+++ server/routes/subjectsRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { protect } from '../middleware/authMiddleware.js'
 import { authorizeRoles } from '../middleware/roleMiddleware.js'
-import { addSubject } from '../controllers/subjectsController.js'
+import { addSubject, getSubject } from '../controllers/subjectsController.js'
 
 const router = express.Router()
 
 router.post('/', protect, authorizeRoles('ADMIN'), addSubject)
+router.get('/:subjectId', protect, authorizeRoles('ADMIN'), getSubject)
 
 export default router
