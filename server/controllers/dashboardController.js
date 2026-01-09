6abde18048e73d65c2700333266b5b265b9d7404
Bemnet-21 <anjejobemnet@gmail.com>
2026-01-09 23:25:59 +0300

    add endpoint to get student performance overall

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -18,3 +18,40 @@ export const getDashboardStats = async (req, res) => {
         res.status(500).json({ message: 'Internal Server Error' })
     }
 }
+
+export const getStudentPerformance = async (req, res) => {
+    const { grade } = req.query
+    if(!grade) return res.status(400).json({ message : "Grade is required" })
+
+    try {
+        const query = `
+            SELECT
+                sub.name AS subject,
+                CAST(ROUND(AVG(g.score)) AS INTEGER) AS average
+            FROM grade g
+            JOIN subject sub ON g.subject_id = sub.id
+            JOIN student s ON g.student_id = s.id
+            JOIN class c ON s.class_id = c.id
+            WHERE c.grade = $1
+            GROUP BY sub.name
+            ORDER BY average DESC;
+        `
+
+        const results = await db.query(query, [grade])
+
+        if(results.rows.length === 0) {
+            return res.status(404).json({ message : "No performance has been found" })
+        }
+
+        res.status(200).json({ 
+            message : "Success",
+            data: results.rows
+         })
+
+
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({ message : "Internal Server Error" })
+    }
+}
--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 import { authorizeRoles } from '../middleware/roleMiddleware.js';
-import { getDashboardStats } from '../controllers/dashboardController.js';
+import { getDashboardStats, getStudentPerformance } from '../controllers/dashboardController.js';
 
 const router = express.Router();
 router.get('/stats', protect, authorizeRoles('ADMIN'), getDashboardStats);
+router.get('/performance', protect, authorizeRoles('ADMIN'), getStudentPerformance)
 
 export default router;
