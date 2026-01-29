00e1270f417046e987b83c01ba26ff65950d5dd1
Bemnet-21 <anjejobemnet@gmail.com>
2026-01-30 00:00:19 +0300

    update pg setup

--- server/db/index.js
+++ server/db/index.js
@@ -5,14 +5,19 @@ dotenv.config()
 const { Pool } = pkg
 
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+    // user: process.env.PG_USER,
+    // host: process.env.PG_HOST,
+    // database: process.env.PG_DATABASE,
+    // password: process.env.PG_PASSWORD,
+    // port: process.env.PG_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 pool.on('connect', () => {
     console.log("Connected to the PostgreSQL database")
 })
+
 
 export default pool
